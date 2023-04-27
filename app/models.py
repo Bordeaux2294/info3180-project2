d09e750cf00d5afbaeaf77470113c291e17d9925
Bordeaux2294 <ford.trishanna876@gmail.com>
2023-04-27 14:09:08 -0500

    Added image

--- app/models.py
+++ app/models.py
@@ -1,1 +1,59 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+import pytz
+from datetime import datetime
+from werkzeug.security import generate_password_hash
+
+class Users(db.Model):
+    # You can use this to change the table name. The default convention is to use
+    # the class name. In this case a class name of UserProfile would create a
+    # user_profile (singular) table, but if we specify __tablename__ we can change it
+    # to `user_profiles` (plural) or some other name.
+    __tablename__ = 'Users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(128))
+    firstname = db.Column(db.String(80))
+    lastname = db.Column(db.String(80))
+    email = db.Column(db.String(120))
+    location = db.Column(db.String(120))
+    biography = db.Column(db.String(255))
+    profile_photo = db.Column(db.String(100))
+    joined_on = db.Column(db.DateTime)
+    
+    def __init__(self, id, username, password, firstname, lastname, email, location, biography, profile_photo, joined_on):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.username = username
+        self.password = generate_password_hash(password, method='pbkdf2:sha256')
+        self.email = email
+        self.id= id
+        self.location = location
+        self.biography = biography
+        self.profile_photo = profile_photo
+        time = datetime.now(pytz.timezone('US/Eastern'))
+        self.joined_on = time
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+    
+class Posts(db.Model):
+    __tablename__ = 'Posts'
+
+    
