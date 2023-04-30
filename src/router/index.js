2130d3241d5b501d5a06e57febc4c1d0ee30fc4f
Orane <orane.henriques@gmail.com>
2023-04-30 14:18:05 -0500

    minor changes

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import RegisterView from '../views/RegisterView.vue'
 import LoginView from '../views/LoginView.vue'
 import PostView from '../views/PostView.vue'
 import BaseView from '../views/BaseView.vue'
-import BaseView from '../views/UserView.vue'
+import UserView from '../views/UserView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
